ddd9b6604dcb9af363b3a91a36d9c7ca906c7cea
Nyaser <61904116+ProjectNyaser@users.noreply.github.com>
2026-07-03 18:45:31 +0800

    fix: update fetch request to follow redirects in cache service

--- src/sw.js
+++ src/sw.js
@@ -242,7 +242,7 @@ async function serveFromCache(request, event) {
   const cached = await cache.match(cacheUrl);
   if (cached) return cached;
   try {
-    const res = await fetch(request);
+    const res = await fetch(request.url, { redirect: "follow" });
     if (res.ok) event?.waitUntil(cache.put(cacheUrl, res.clone()));
     return res;
   } catch {
